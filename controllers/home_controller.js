043cc5e34b50793ac4f6d1500d1c985e76515a84
mohdnuman <mohammadnuman123@gmail.com>
2022-02-13 17:38:39 +0530

    added post list

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,17 @@
 const User=require('../models/user');
+const Post=require('../models/post');
+
 
 module.exports.home=async function(req,res){
     try{
         let users=await User.find({});
+        let posts=await Post.find({})
+        .sort('-createdAt')
+        .populate('user');
 
         return res.render('home',{
-        users:users
+        users:users,
+        posts:posts
         });
 
     }catch(err){
