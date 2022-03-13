2b5a1c06c9e3cb95e43c3e3ab0975ce2929ef8e4
mohdnuman <mohammadnuman123@gmail.com>
2022-03-13 17:17:50 +0530

    removed some bugs

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const homeController = require("../controllers/home_controller.js");
-router.get("/", homeController.home);
-router.use("/user", require("./user.js"));
-router.use("/connection", require("./connection.js"));
-router.use("/post", require("./post.js"));
-router.use("/api", require("./api/index"));
+// const homeController = require("../controllers/home_controller.js");
+// router.get("/", homeController.home);
+// router.use("/user", require("./user.js"));
+// router.use("/connection", require("./connection.js"));
+// router.use("/post", require("./post.js"));
+// router.use("/api", require("./api/index"));
 
 const homeController = require("../controllers/home_controller.js");
 const coursePageController = require("../controllers/coursePage_controller");
